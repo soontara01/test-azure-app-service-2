4eeea3b13806407f796a140e26c2f032ee6cbe4e
Soontara Rachapud <soontarr@ais.co.th>
2023-03-05 17:10:56 +0700

    'x'

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -1,6 +1,7 @@
 package com.example.springboot;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,6 +10,11 @@ public class HelloController {
 	@GetMapping("/noom")
 	public String index() {
 		return "Greetings from Spring Boot!xxx1";
+	}
+	
+	@RequestMapping("/")
+	String sayHello() {
+		return "Hello World!";
 	}
 
 }
